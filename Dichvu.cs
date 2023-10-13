2dddfd179000ef6a4dca07a57504078d40cae602
Hà My <145450102+hami308@users.noreply.github.com>
2023-10-13 10:13:09 +0700

    Add files via upload

---
+++ Dichvu.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiaoDien_qlpks
+{
+    public partial class Dichvu : Form
+    {
+        public Dichvu()
+        {
+            InitializeComponent();
+            loaddichvulist();
+        }
+        void loaddichvulist()
+        {
+            try
+            {
+                string connectionSTR = @"Data Source=LAPTOP-JKMABAVK\TESTSQL;Initial Catalog=QUANLYPHONGKHACHSAN;Integrated Security=True;TrustServerCertificate=true;";
+                SqlConnection connection = new SqlConnection(connectionSTR);
+                string query = "SELECT * FROM [dbo].[Table_SANPHAM]";
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                DataTable data = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+                dtgvdichvu.DataSource = data;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+        }
+        private void Dichvu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btcapnhap_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void xoa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtgvdichvu_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ formlogin.cs
@@ -0,0 +1,73 @@
+﻿namespace GiaoDien_qlpks
+{
+    public partial class FormDangnhap : System.Windows.Forms.Form
+    {
+        public FormDangnhap()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Textbox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Textbox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "") || textBox2.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập thông tin", "THÔNG BÁO");
+            }
+            else
+            {
+                if ((textBox1.Text == "a") && (textBox2.Text == "1"))
+                {
+                    MessageBox.Show("Đăng nhập thành công", "Thông báo");
+                    Form Trangchu = new Trangchu();
+                    this.Hide();//ẩn cửa sổ hiện tại, cửa sổ chứa đoạn mã này
+                    Trangchu.ShowDialog();
+                    this.Show();//quay lại form bị ẩn bởi lệnh hide()
+                }
+                else
+                    MessageBox.Show("Vui lòng nhập lại thông tin", "THÔNG BÁO");
+
+            }
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormDangnhap_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
